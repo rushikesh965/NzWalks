3f50f9bf3b6e8f13799f31945d48698402276e5e
rushikeshmahale901@gmail.com <mahalehrishikesh707@gmail.com>
2024-07-28 00:55:21 +0530

    End of Section 2

---
+++ NzWalks/NzWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalks.API.Models.Domain;
+
+namespace NzWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base (options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
